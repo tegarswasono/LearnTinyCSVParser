e026ef58639e2cf0c250400a3db0ba16cdd93fb3
tegarsweefer <tegar.s@weefer.co.id>
2020-05-14 14:41:18 +0700

    first commit

---
+++ Controllers/ReadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnTinyCSVParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Oke");
+        }
+    }
+}
