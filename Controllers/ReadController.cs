8fbc905ff57855e2db0be7eceef6cd87d1ba9655
tegarsweefer <tegar.s@weefer.co.id>
2020-05-14 14:57:22 +0700

    success read CSV file

--- Controllers/ReadController.cs
+++ Controllers/ReadController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using LearnTinyCSVParser.Helpers;
+using LearnTinyCSVParser.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TinyCsvParser;
 
 namespace LearnTinyCSVParser.Controllers
 {
@@ -14,6 +18,17 @@ namespace LearnTinyCSVParser.Controllers
         [HttpGet]
         public ActionResult Get()
         {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
+            CsvProductMapping csvMapper = new CsvProductMapping();
+            CsvParser<Product> csvParser = new CsvParser<Product>(csvParserOptions, csvMapper);
+            var products = csvParser.ReadFromFile(@"C:\Project\testing2\"+ "Book1.csv", Encoding.ASCII).ToList();
+            //products.RemoveAt(0);
+
+            foreach (var product in products)
+            {
+                var result = product.Result;
+            }
+
             return Ok("Oke");
         }
     }
---
+++ Helpers/CsvProductMapping.cs
@@ -0,0 +1,19 @@
+﻿using LearnTinyCSVParser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyCsvParser.Mapping;
+
+namespace LearnTinyCSVParser.Helpers
+{
+    public class CsvProductMapping : CsvMapping<Product>
+    {
+        public CsvProductMapping() : base()
+        {
+            MapProperty(0, x => x.Name);
+            MapProperty(1, x => x.Description);
+            MapProperty(2, x => x.Price);
+        }
+    }
+}
