d815692c961c15b45aa3fec4360ec372c5be5561
tegarsweefer <tegar.s@weefer.co.id>
2020-05-19 17:14:28 +0700

    success create csv file

---
+++ Controllers/WriteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearnTinyCSVParser.Helpers;
+using LearnTinyCSVParser.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnTinyCSVParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WriteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            var stream = System.IO.File.CreateText("Files/csvFile.csv");
+            stream.WriteLine("Name,Address");
+            stream.WriteLine("Joko,Malang");
+            stream.WriteLine("Joni,Surabaya");
+            stream.Close();
+            return Ok();
+        }
+
+        [HttpGet("FromObject")]
+        public ActionResult FromObject()
+        {
+            string headerCsv = "Name,Address";
+            List<Product> products = GetProducts();
+
+            var stream = System.IO.File.CreateText("Files/csvFile.csv");
+            CreateCSV.Create(stream, headerCsv, products);
+            stream.Close();
+
+
+            return Ok();
+        }
+        private List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product
+            {
+                Name = "Dani",
+                Description = "desc1"
+            });
+            products.Add(new Product
+            {
+                Name = "Ari",
+                Description = "desc1"
+            });
+            return products;
+        }
+    }
+}
---
+++ Helpers/CreateCSV.cs
@@ -0,0 +1,21 @@
+﻿using LearnTinyCSVParser.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnTinyCSVParser.Helpers
+{
+    public static class CreateCSV
+    {
+        public static void Create(StreamWriter stream, string header, List<Product> products)
+        {
+            stream.WriteLine(header);
+            foreach (var product in products)
+            {
+                stream.WriteLine(product.Name + "," + product.Description);
+            }
+        }
+    }
+}
